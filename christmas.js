0c2e95f6dbd46a24fdf5689fdeee5a3233761615
Jacob Spence <jspence@basecampcodingacademy.org>
2016-12-02 15:18:20 -0600

    added images and a random tree generatior in javascript.

--- christmas.js
+++ christmas.js
@@ -3,3 +3,9 @@ $.getJSON("http://api.wunderground.com/api/fd843d28868e77c2/conditions/q/MS/Wate
   weather = json.data;
   console.log(json)
 });
+var trees = ["tree1.png", "tree2.png", "tree3.jpeg"];
+function tree(){
+  var which = trees[Math.floor(Math.random() * 3)];
+  $("#treebox").attr("src", "img/" + which);
+}
+$(document).ready(tree);
