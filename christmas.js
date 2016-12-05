82ffa9deb9f69ecff1a2a57584296854afb8d7a7
Jacob Spence <jspence@basecampcodingacademy.org>
2016-12-05 15:01:52 -0600

    made it were you can choose between snowman or christmas tree

--- christmas.js
+++ christmas.js
@@ -1,11 +1,44 @@
-
-$.getJSON("http://api.wunderground.com/api/fd843d28868e77c2/conditions/q/MS/Water_Valley.json").done(function(json){
-  weather = json.data;
-  console.log(json)
+$.getJSON("http://api.wunderground.com/api/fd843d28868e77c2/conditions/q/MS/Water_Valley.json").done(function(json) {
+    weather = json.data;
+    console.log(json)
 });
+
 var trees = ["tree1.png", "tree2.png", "tree3.jpeg"];
-function tree(){
-  var which = trees[Math.floor(Math.random() * 3)];
-  $("#treebox").attr("src", "img/" + which);
+
+function tree() {
+    var which = trees[Math.floor(Math.random() * 3)];
+    $("#treebox").attr("src", "img/" + which);
+};
+var pic = $("#treebox")
+
+function show_choice(){
+  pic.empty()
+  if ($('#myList').val() == 'christmas tree'){
+  tree()
 }
-$(document).ready(tree);
+else {
+   $("#treebox").attr("src", "img/snowman.png")
+};
+}
+
+
+
+$(document).ready(function(){
+$("#submit").click(function(){
+show_choice()
+});
+});
+
+
+
+
+// if (('#myList') == 'christmas tree') {
+//     $(documemt).ready(tree);
+// };
+//
+// if(('#myList') == 'snowman') {
+//     $(document).ready(function() {
+//         $("#treebox").attr("src", "img/snoman.png")
+//     });
+//
+// };
