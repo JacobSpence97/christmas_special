c96f375166c97c7c24f18ec485c6dd57670d7125
Jacob Spence <jspence@basecampcodingacademy.org>
2016-12-09 14:54:48 -0600

    fixed some minor bugs and added more ornaments

--- christmas.js
+++ christmas.js
@@ -9,9 +9,9 @@ var songs = ["adam.mp3", "addey.mp3", "dustin.mp3", "jacob.mp3",
 
 var trees = ["tree1.png", "tree2.png", "tree3.png"];
 
-var smd = ["broom1.png", "broom2.png", "hat1.png", "hat2.png", "snowmanface.png"];
+var smd = ["broom1.png", "broom2.png", "hat1.png", "hat2.png", "face.png"];
 
-var ctd = ["ornament1.png", "ornament2.png", "ornament3.png", "star1.png", "star2.png", "star3.png"];
+var ctd = ["ornament1.png", "ornament1.png", "ornament1.png", "ornament2.png", "ornament3.png","ornament3.png","ornament3.png", "star1.png", "star2.png", "star3.png"];
 
 function tree() {
     var which = trees[Math.floor(Math.random() * 3)];
@@ -42,8 +42,9 @@ function christmas_tree() {
 function show_choice() {
     pic.empty();
     if ($('#myList').val() == 'christmas tree') {
-        tree()
+        tree();
         christmas_tree();
+
     } else {
         $("#treebox").attr("src", "img/snowman.png");
         snowman();
@@ -66,6 +67,8 @@ $(document).ready(function() {
     $("#submit").click(function() {
         $("#items").empty();
         show_choice();
+        song();
+        item.show();
     });
     $("#grinch").click(function() {
         grinch();
