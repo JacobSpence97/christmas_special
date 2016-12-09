e62a5ea65b36476df7c7ed592359d2eb3dff5100
Jacob Spence <jspence@basecampcodingacademy.org>
2016-12-09 13:21:41 -0600

    Resized the pictures of the items. then created a div to put them in and made the items draggable. completed projrect as of now

--- christmas.js
+++ christmas.js
@@ -3,57 +3,71 @@
 //     console.log(json)
 // });
 var songs = ["adam.mp3", "addey.mp3", "dustin.mp3", "jacob.mp3",
-              "jamesh.mp3", "jamess.mp3","rock.mp3", "martain.mp3", "nate.mp3",
-              "nicloe.mp3","onna.mp3", "randoms.mp3", "ricky.mp3", "sean.mp3"]
+    "jamesh.mp3", "jamess.mp3", "rock.mp3", "martain.mp3", "nate.mp3",
+    "nicloe.mp3", "onna.mp3", "randoms.mp3", "ricky.mp3", "sean.mp3"
+];
 
-var trees = ["tree1.png", "tree2.png", "tree3.jpeg"];
+var trees = ["tree1.png", "tree2.png", "tree3.png"];
 
-var smd = ["broom1.png", "broom2.png", "hat1.png", "hat2.png", "snowmanface.png"]
+var smd = ["broom1.png", "broom2.png", "hat1.png", "hat2.png", "snowmanface.png"];
 
-var ctd = ["ornament1.png", "ornament2.png", "ornament3.png", "star1.png", "star2.png", "star3.png"]
+var ctd = ["ornament1.png", "ornament2.png", "ornament3.png", "star1.png", "star2.png", "star3.png"];
 
 function tree() {
     var which = trees[Math.floor(Math.random() * 3)];
     $("#treebox").attr("src", "img/" + which);
 };
-var pic = $("#treebox")
+var pic = $("#treebox");
+var item = $("#items");
 
-function song(){
-  var which = songs[Math.floor(Math.random() * 14)]
-  $("#music").attr("src", "sound/" + which);
-  console.log(which)
+function song() {
+    var which = songs[Math.floor(Math.random() * 14)]
+    $("#music").attr("src", "sound/" + which);
+    console.log(which)
 };
 
+function snowman() {
+    for (x = 0; x < smd.length; x++) {
+        $("#items").append("<img src=img/" + smd[x] + " ><\img>");
+        $("img").draggable();
+    }
+};
 
+function christmas_tree() {
+    for (x = 0; x < ctd.length; x++) {
+        $("#items").append("<img src=img/" + ctd[x] + " ><\img>");
+        $("img").draggable();
+    }
+};
 function show_choice() {
     pic.empty();
     if ($('#myList').val() == 'christmas tree') {
         tree()
+        christmas_tree();
     } else {
-        $("#treebox").attr("src", "img/snowman.png")
-    };
-};
-
-function grinch(){
-  pic.empty();
-  $("#music").attr("src", "sound/grinch.mp3");
-  $("#treebox").attr("src", "img/grinch.jpg").hide().fadeIn(9999)
-
+        $("#treebox").attr("src", "img/snowman.png");
+        snowman();
+    }
 };
 
 
-    $(document).ready(function() {
-      song();
-        $("#submit").click(function() {
-            show_choice();
-        });
-        $("#grinch").click(function(){
-          grinch();
 
-        });
+function grinch() {
+    pic.empty();
+    item.hide();
+    $("#music").attr("src", "sound/grinch.mp3");
+    $("#treebox").attr("src", "img/grinch.jpg").hide().fadeIn(9999);
+
+};
+
+
+$(document).ready(function() {
+    song();
+    $("#submit").click(function() {
+        $("#items").empty();
+        show_choice();
     });
-
-    //
-    // $(function() {
-    //     $("#items").draggable();
-    // });
+    $("#grinch").click(function() {
+        grinch();
+    });
+});
