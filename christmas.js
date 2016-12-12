2c1d6b9c2cd3ba79d9db37af9dcb1082e510d32e
Jacob Spence <jspence@basecampcodingacademy.org>
2016-12-12 10:48:06 -0600

    linked an api and made the weather affect the back ground of the page

--- christmas.js
+++ christmas.js
@@ -1,7 +1,3 @@
-// $.getJSON("http://api.wunderground.com/api/fd843d28868e77c2/conditions/q/MS/Water_Valley.json").done(function(json) {
-//     weather = json.data;
-//     console.log(json)
-// });
 var songs = ["adam.mp3", "addey.mp3", "dustin.mp3", "jacob.mp3",
     "jamesh.mp3", "jamess.mp3", "rock.mp3", "martain.mp3", "nate.mp3",
     "nicloe.mp3", "onna.mp3", "randoms.mp3", "ricky.mp3", "sean.mp3"
@@ -11,7 +7,7 @@ var trees = ["tree1.png", "tree2.png", "tree3.png"];
 
 var smd = ["broom1.png", "broom2.png", "hat1.png", "hat2.png", "face.png"];
 
-var ctd = ["ornament1.png", "ornament1.png", "ornament1.png", "ornament2.png", "ornament3.png","ornament3.png","ornament3.png", "star1.png", "star2.png", "star3.png"];
+var ctd = ["ornament1.png", "ornament1.png", "ornament1.png", "ornament2.png", "ornament3.png", "ornament3.png", "ornament3.png", "star1.png", "star2.png", "star3.png"];
 
 function tree() {
     var which = trees[Math.floor(Math.random() * 3)];
@@ -39,6 +35,7 @@ function christmas_tree() {
         $("img").draggable();
     }
 };
+
 function show_choice() {
     pic.empty();
     if ($('#myList').val() == 'christmas tree') {
@@ -62,7 +59,18 @@ function grinch() {
 };
 
 
+
 $(document).ready(function() {
+    $.getJSON("http://api.wunderground.com/api/fd843d28868e77c2/conditions/q/MS/Water_Valley.json").done(function(json) {
+        weather = json;
+        console.log(json)
+        if (weather.current_observation.temp_f <= 60) {
+            $("body").css('background-image', 'url(img/snow.jpg)');
+        }
+         else {
+            $("body").css('background-image', 'url(img/sun.jpg)');
+        }
+    });
     song();
     $("#submit").click(function() {
         $("#items").empty();
