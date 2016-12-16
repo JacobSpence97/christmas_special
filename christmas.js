fadddbd4d92cffec97e87e3614741f2e8fe80c78
Jacob Spence <jspence@basecampcodingacademy.org>
2016-12-16 10:45:15 -0600

    addded more tree decoration

--- christmas.js
+++ christmas.js
@@ -7,7 +7,7 @@ var trees = ["tree1.png", "tree2.png", "tree3.png"];
 
 var smd = ["broom1.png", "broom2.png", "hat1.png", "hat2.png", "face.png","arm1.png","arm2.png", "mitten.png", "right.png", "buttons.png"];
 
-var ctd = ["ornament1.png", "ornament1.png", "ornament1.png", "ornament2.png", "ornament3.png", "ornament3.png", "ornament3.png", "star1.png", "star2.png", "star3.png", "gift.png", "gift2.png", "present.png"];
+var ctd = ["ornament1.png", "ornament1.png", "ornament1.png", "ornament2.png", "ornament3.png", "ornament3.png", "ornament3.png", "star1.png", "star2.png", "star3.png", "gift.png", "gift2.png", "present.png","gift4.png","candy.png", "candy.png", "lights.gif", "lights.gif", "lights.gif", "lights.gif"];
 
 function tree() {
     var which = trees[Math.floor(Math.random() * 3)];
